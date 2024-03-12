bb03b89403b6ce994f477977154998c6f8d95783
Mahmoud K. El-Shafey <m.elshafaay66@gmail.com>
2024-03-12 15:02:12 +0200

    Add files via upload

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Add New Transaction</h3>
+  <form id="form">
+    <div class="form-control">
+      <label for="text">Text</label>
+      <input type="text" id="text" placeholder="Enter Text..." />
+    </div>
+    <div class="form-control">
+      <label for="amount"
+        >Amount<br />
+        <sub> ➖ Expenses | ➕ Income</sub></label
+      >
+      <input type="number" id="amount" placeholder="Enter Amount..." />
+    </div>
+    <button class="btn">Add Transaction</button>
+  </form>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li class="minus">Phone Bill <span>-$400</span> <button class="delete-btn">x</button></li>
+    <li class="plus">Paycheck<span>+$1100</span> <button class="delete-btn">x</button></li>
+  </ul>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
